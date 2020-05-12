61125f2363adb382375f5056a04eb46d3f00f5b9
Coderbora <coderbora@gmail.com>
2020-05-12 10:50:57 +0300

    register command cooldown

--- cogs/registrations.py
+++ cogs/registrations.py
@@ -3,7 +3,7 @@ from database import Database
 
 import uuid
 
-import datetime
+from datetime import datetime, timedelta
 
 import json
 
@@ -25,19 +25,28 @@ class Registrations(commands.Cog):
             return
 
         db.select(table="register_requests", discord_id=ctx.author.id)
-        if db.fetchone():
-            db.delete(table="register_requests", discord_id=ctx.author.id)
+        data = db.fetchone()
+
+        if data:
+            if datetime.strptime(data[2], "%Y-%m-%d %H:%M:%S.%f") < (datetime.now() - timedelta(days=1)):
+                db.delete(table="register_requests", discord_id=ctx.author.id)
+            else:
+                await ctx.send(
+                    "Bir önceki katılma isteğinizin süresi dolmadı. Lütfen DM'deki geçerli linke tıklayınız.")
+                return
 
         key = uuid.uuid4().hex[:32]
-        db.insert("register_requests", ctx.author.id, key, datetime.datetime.now())
+        db.insert("register_requests", ctx.author.id, key, datetime.now())
 
         with open('config.json') as config_file:
-            data = json.load(config_file)
+            cfg = json.load(config_file)
 
         try:
             await ctx.author.send(
-                "Kaydınızı tamamlamak için " + data["website"] + "/completeRegistration?key=" + key + " adresinden osu! hesabınızı tanımlayın! Bu link yalnızca 24 saatliğine geçerlidir.")
+                "Kaydınızı tamamlamak için " + cfg[
+                    "website"] + "/completeRegistration?key=" + key + " adresinden osu! hesabınızı tanımlayın! Bu link yalnızca 24 saatliğine geçerlidir.")
         except:
+            db.delete(table="register_requests", discord_id=ctx.author.id)
             await ctx.send("DM göndermede hata oluştu. Lütfen gizlilik ayarlarınızdan DM izinlerinizi kontrol edin.")
             return
         await ctx.send("Kaydınızı tamamlamak için lütfen DM'nize gelen linke tıklayın.")
